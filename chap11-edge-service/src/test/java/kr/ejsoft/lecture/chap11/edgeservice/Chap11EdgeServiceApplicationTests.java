cf6b17fd3f3f25d1aae7111c8df0562e9e908688
김영기 <ygkim@innogrid.com>
2025-02-13 12:34:47 +0900

    chore: chap12 보안 및 인가 처리

--- chap11-edge-service/src/test/java/kr/ejsoft/lecture/chap11/edgeservice/Chap11EdgeServiceApplicationTests.java
+++ chap11-edge-service/src/test/java/kr/ejsoft/lecture/chap11/edgeservice/Chap11EdgeServiceApplicationTests.java
@@ -27,4 +27,8 @@ class Chap11EdgeServiceApplicationTests {
 	@Test
 	void verifyThatSpringContextLoads() {
 	}
+
+	@Test
+	void contextLoads() {
+	}
 }
--- chap12-edge-service/src/test/java/kr/ejsoft/lecture/chap12/edgeservice/Chap12EdgeServiceApplicationTests.java
+++ chap12-edge-service/src/test/java/kr/ejsoft/lecture/chap12/edgeservice/Chap12EdgeServiceApplicationTests.java
@@ -27,4 +27,8 @@ class Chap12EdgeServiceApplicationTests {
 	@Test
 	void verifyThatSpringContextLoads() {
 	}
+
+	@Test
+	void contextLoads() {
+	}
 }
